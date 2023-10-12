225e6e2f104b241a6337eb1e7d96db71835a24ee
ctorres0312 <113136320+ctorres0312@users.noreply.github.com>
2023-10-12 16:14:50 -0400

    columns

--- app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -1,11 +1,11 @@
 "use client"
 
 
-import { Heading } from "@/components/ui/heading"
-import { Separator } from "@/components/ui/separator"
-import React from "react"
-import { OrderColumn, columns } from "./columns"
-import { DataTable } from "@/components/ui/data-table"
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import React from "react";
+import { OrderColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
 
 
 interface OrderClientProps {
